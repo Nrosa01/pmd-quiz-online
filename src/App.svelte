67f0355724b522359d769f9672e16005f3116f53
Rioni <nrosa01@ucm.es>
2022-11-02 07:42:39 +0100

    Added start menu to force click and get sound

--- src/App.svelte
+++ src/App.svelte
@@ -3,13 +3,19 @@
   import { store, loadData } from "./assets/store.js";
   import { onMount } from "svelte";
   import { fade } from "svelte/transition";
+  import { isTouchEnabled, getLanguage } from "./assets/utils.js";
+  import { transition_in } from "svelte/internal";
 
   $store.numQuestions = 10;
   let musicBg = null;
   let loaded = loadData();
+  let isPlaying = false;
+  let canStart = false;
+  let finishedTransition = false;
+  let isSpanish = getLanguage() === "es";
 
   onMount(() => {
-    musicBg.volume = 0.2;
+    musicBg.volume = 0.1;
   });
 
   let pokemons = [
@@ -47,7 +53,7 @@
   {#each pokemons as pokemon}
     <img src="img/pokemonicons/{pokemon}.png" alt="" />
   {/each}
-  <img src="img/others/pmdtextbox" alt=""/>
+  <img src="img/others/pmdtextbox" alt="" />
 </section>
 
 <section>
@@ -62,20 +68,49 @@
 {#await loaded}
   <div
     class="flex flex-col justify-center items-center text-center h-screen w-screen bg-black/50 z-10">
-    <h1 class="text-8xl text-white animate-pulse">Loading...</h1>
+    <h1 class="text-8xl text-white animate-pulse">{isSpanish ? "Cargando..." : "Loading..."}</h1>
   </div>
 {:then data}
   <main class="relative" transition:fade>
     <section>
-      <div class="flex flex-col flex-wrap h-screen justify-end bg-none">
-        <QuestionSystem dataExt="{data}" />
-      </div>
+      {#if canStart}
+        {#if finishedTransition}
+          <div
+            class="flex flex-col flex-wrap h-screen justify-end bg-none"
+            transition:fade>
+            <QuestionSystem dataExt="{data}" />
+          </div>
+        {/if}
+      {:else}
+        <div
+          class="flex flex-col flex-wrap h-screen justify-end bg-none"
+          on:pointerdown="{() => {
+            canStart = true;
+          }}"
+          transition:fade
+          on:outroend="{() => {
+            if (!isPlaying) {
+              musicBg.play();
+              isPlaying = true;
+            }
+            finishedTransition = true;
+          }}">
+          <div
+            class="flex flex-col justify-center items-center text-center h-screen w-screen bg-black/50 z-10">
+            <h1 class="text-8xl text-white animate-pulse">
+              {isTouchEnabled()
+                ? data.strings["TouchToStart"]
+                : data.strings["ClickToStart"]}
+            </h1>
+          </div>
+        </div>
+      {/if}
     </section>
   </main>
 {:catch}
   <div
     class="flex flex-col justify-center items-center text-center h-screen w-screen bg-black/50 z-10"
     transition:fade>
-    <h1 class="text-8xl text-white animate-pulse">Error promise rejected</h1>
+    <h1 class="text-8xl text-white animate-pulse">{isSpanish ? "Error al cargar :(" : "Error promise rejected :("}</h1>
   </div>
 {/await}
