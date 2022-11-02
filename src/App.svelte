a47ec74245ec638b4b54f3eefa4bc642dbac860b
Rioni <nrosa01@ucm.es>
2022-11-02 02:42:58 +0100

    Promises written different

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,34 @@
 <script>
   import QuestionSystem from "./lib/QuestionSystem.svelte";
-  import { store } from "./assets/store.js";
+  import { store, loadData } from "./assets/store.js";
   import { onMount } from "svelte";
 
   $store.numQuestions = 10;
   let musicBg = null;
+  let loaded = loadData();
 
   onMount(() => {
     musicBg.volume = 0.2;
   });
 </script>
 
-{#if $store.questions && $store.natures}
+<audio
+bind:this="{musicBg}"
+src="audio/quiz-music.ogg"
+type="audio/ogg"
+controls="{false}"
+loop
+autoplay
+preload="auto"></audio>
+
+{#await loaded}
+  <div
+    class="flex flex-col justify-center items-center text-center h-screen w-screen bg-black/90 z-10">
+    <h1 class="text-8xl text-white">Loading...</h1>
+  </div>
+{:then data}
   <main class="relative">
     <section>
-      <audio
-        bind:this="{musicBg}"
-        src="audio/quiz-music.ogg"
-        type="audio/ogg"
-        controls="{false}"
-        loop
-        autoplay
-        preload="auto"></audio>
-
       <div
         class="fixed h-screen w-screen justify-center bg-cover brightness-75 infinite-scroll-right">
       </div>
@@ -33,13 +39,8 @@
 
     <section>
       <div class="flex flex-col flex-wrap h-screen justify-end bg-none">
-        <QuestionSystem />
+        <QuestionSystem dataExt={data}/>
       </div>
     </section>
   </main>
-{:else}
-  <div
-    class="flex flex-col justify-center items-center text-center h-screen w-screen bg-black/90 z-10">
-    <h1 class="text-8xl text-white">Loading...</h1>
-  </div>
-{/if}
+{/await}
